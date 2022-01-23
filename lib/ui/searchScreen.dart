416dfc48c8a8f29886503829db9616f7420e1e3a
sardonicscallop <kantor2@o2.pl>
2022-01-23 14:47:16 +0100

    Polished searching UI and added checkboxes

--- lib/ui/searchScreen.dart
+++ lib/ui/searchScreen.dart
@@ -4,36 +4,174 @@ import 'package:http/http.dart' as http;
 import '/models.dart' as Models;
 import '/networking.dart';
 
+class SearchWithinCheckbox extends StatelessWidget {
+  const SearchWithinCheckbox({
+    Key? key,
+    required this.title,
+    required this.padding,
+    required this.value,
+    required this.onChanged,
+    this.checkColor,
+    this.fillColor,
+  }) : super(key: key);
 
-class SearchScreen extends StatelessWidget {
-  const SearchScreen({Key? key}) : super(key: key);
+
+  final Widget title;
+  final EdgeInsets padding;
+  final bool value;
+  final ValueChanged<bool> onChanged;
+  final Color? checkColor;
+  final MaterialStateProperty<Color>? fillColor;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text("Search"),
-      ),
-      body: FutureBuilder<List<Models.Degree>>(
-        future: fetchDegrees(http.Client()),
-        builder: (context, snapshot) {
-          if (snapshot.hasError) {
-            print(snapshot.toString());
-            return const Center(
-              child: Text('An error has occurred!'),
-            );
-          } else if (snapshot.hasData) {
-            return DegreesList(degrees: snapshot.data!);
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
+    return InkWell(
+      onTap: () {
+        onChanged(!value);
+      },
+      child: Padding(
+        padding: padding,
+        child: Column(
+          children: [
+            Row(
+              children: <Widget>[
+                Checkbox(
+                  value: value,
+                  onChanged: (bool? newValue) {
+                    onChanged(newValue!);
+                  },
+                  checkColor: checkColor,
+                  fillColor: fillColor,
+                ),
+                title,
+              ],
+            )
+          ],
+        )
+
       ),
     );
   }
 }
+
+class SearchScreen extends StatefulWidget {
+  const SearchScreen({Key? key}) : super(key: key);
+
+  @override
+  _SearchScreenState createState() => _SearchScreenState();
+}
+
+class _SearchScreenState extends State<SearchScreen> {
+
+  final searchBoxController = TextEditingController();
+
+  bool _searchDegrees = true;
+  bool _searchTeachers = true;
+  bool _searchRooms = true;
+
+
+  @override
+  void initState() {
+    super.initState();
+    searchBoxController.addListener(_showSearchResults);
+  }
+
+  @override
+  void dispose() {
+    searchBoxController.dispose();
+    super.dispose();
+  }
+
+  void _showSearchResults() {
+    // todo sth there
+    print(searchBoxController.text);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: TextField(
+            controller: searchBoxController,
+            cursorColor: Colors.white,
+            style: TextStyle(color: Colors.white),
+            decoration: InputDecoration(
+              hintText: "Search",
+              hintStyle: TextStyle(color: Colors.white),
+              prefixIcon: Icon(Icons.search, color: Colors.white),
+              enabledBorder: InputBorder.none,
+              focusedBorder: UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
+              focusColor: Colors.white,
+              ),
+            ),
+          bottom: PreferredSize(
+            preferredSize: Size.fromHeight(60.0),
+            child: Row(
+              /*scrollDirection: Axis.horizontal,
+              shrinkWrap: true, */
+              children: [
+                SearchWithinCheckbox(
+                  value: _searchDegrees,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _searchDegrees = value!;
+                    });
+                    },
+                  title: Text("Degrees", style: TextStyle(color: Colors.white)),
+                  padding: const EdgeInsets.all(5.0),
+                  fillColor: MaterialStateProperty.all(Colors.white),
+                  checkColor: Colors.blue,
+                ),
+                SearchWithinCheckbox(
+                  value: _searchTeachers,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _searchTeachers = value!;
+                    });
+                  },
+                  title: Text("Teachers", style: TextStyle(color: Colors.white)),
+                  padding: const EdgeInsets.all(5.0),
+                  fillColor: MaterialStateProperty.all(Colors.white),
+                  checkColor: Colors.blue,
+                ),
+                SearchWithinCheckbox(
+                  value: _searchRooms,
+                  onChanged: (bool? value) {
+                    setState(() {
+                      _searchRooms = value!;
+                    });
+                  },
+                  title: Text("Rooms", style: TextStyle(color: Colors.white)),
+                  padding: const EdgeInsets.all(5.0),
+                  fillColor: MaterialStateProperty.all(Colors.white),
+                  checkColor: Colors.blue,
+                ),
+              ]
+            )
+          )
+        ),
+        body:
+          FutureBuilder<List<Models.Degree>>(
+            future: fetchDegrees(http.Client()),
+            builder: (context, snapshot) {
+              if (snapshot.hasError) {
+                print(snapshot.toString());
+                return Center(
+                  child: Text('Error: ' + snapshot.error.toString()),
+                );
+              } else if (snapshot.hasData) {
+                return DegreesList(degrees: snapshot.data!);
+              } else {
+                return const Center(
+                  child: CircularProgressIndicator(),
+                );
+              }
+            },
+          ),
+    );
+  }
+}
+
 
 class DegreesList extends StatelessWidget {
   const DegreesList({Key? key, required this.degrees}) : super(key: key);
