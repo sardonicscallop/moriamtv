31e9fcf1acd13be2ae0297f07878b28b55a0fe46
sardonicscallop <kantor2@o2.pl>
2022-01-23 14:56:56 +0100

    Created new functions fetch... and parse... for Teachers and Rooms

--- lib/networking.dart
+++ lib/networking.dart
@@ -26,3 +26,37 @@ List<Models.Degree> parseDegrees(String responseBody)
 
   return parsed['result']['array'].map<Models.Degree>((json) => Models.Degree.fromJson(json)).toList();
 }
+
+
+Future<List<Models.Teacher>> fetchTeachers(http.Client client) async
+{
+  final response = await client
+      .get(Uri.parse(moriaApiAddress + "/students_list"));
+
+  // Using the compute function to run in a separate isolate.
+  return compute(parseTeachers, Utf8Decoder().convert(response.bodyBytes));
+}
+
+List<Models.Teacher> parseTeachers(String responseBody)
+{
+  final parsed = jsonDecode(responseBody);
+
+  return parsed['result']['array'].map<Models.Teacher>((json) => Models.Teacher.fromJson(json)).toList();
+}
+
+
+Future<List<Models.Room>> fetchRooms(http.Client client) async
+{
+  final response = await client
+      .get(Uri.parse(moriaApiAddress + "/room_list"));
+
+  // Using the compute function to run in a separate isolate.
+  return compute(parseRooms, Utf8Decoder().convert(response.bodyBytes));
+}
+
+List<Models.Room> parseRooms(String responseBody)
+{
+  final parsed = jsonDecode(responseBody);
+
+  return parsed['result']['array'].map<Models.Room>((json) => Models.Room.fromJson(json)).toList();
+}
